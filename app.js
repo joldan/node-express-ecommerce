87fa33415f7df0d405ab051e70bd0703e9bbd948
joldan <joldan@gmail.com>
2020-07-23 22:23:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use( (req, res, next) =>{
+    // res.status(404).send('<h1>Oooops, we found a place where nothing exists</h1>')
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    }); 
+
+
+app.listen(3000)
---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => { 
+        // console.log('In yet another the middleware')
+        res.sendFile(path.join(rootDir,'views','add-product.html'));
+        // res.send('<form action="/admin/product" method="POST"><input  type="text" name="title" type="submit"><button>Add Product</button></form>');
+});
+
+router.post('/product', (req, res, next)=> {
+        console.log(req.body);
+        res.redirect('/');
+});
+
+module.exports = router;
