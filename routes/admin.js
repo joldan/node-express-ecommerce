dcd07987d121a052b8d65797c563c6fb393f78f2
joldan <joldan@gmail.com>
2020-07-27 17:54:28 -0300

    improved UI

--- routes/admin.js
+++ routes/admin.js
@@ -5,15 +5,18 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => { 
         // console.log('In yet another the middleware')
         res.sendFile(path.join(rootDir,'views','add-product.html'));
         // res.send('<form action="/admin/product" method="POST"><input  type="text" name="title" type="submit"><button>Add Product</button></form>');
 });
 
-router.post('/product', (req, res, next)=> {
-        console.log(req.body);
+router.post('/add-product', (req, res, next)=> {
+        products.push({title : req.body.title})
         res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 const router = express.Router()
+
 
 router.get('/', (req, res, next) => { 
     // console.log('In yet another the middleware')
     // res.send("<h1>Hello from express</h1>");
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir ,'views', 'shop.html'));
 });
 
