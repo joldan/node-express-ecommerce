7ca83096975c0c7903df51b50b65123f66a75580
Joaquin Oldan <joldan@gmail.com>
2020-09-08 08:00:11 -0300

    added sequelize

--- models/product.js
+++ models/product.js
@@ -1,31 +1,5 @@
-const db = require('../util/database')
-const Cart = require('./cart')
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
 
-//product model class
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price){
-        this.id = id;
-        this.title = title;
-        this.imageUrl= imageUrl,
-        this.description = description,
-        this.price = price
-    }
-
-    save (){
-        return db.execute('INSERT INTO products (title, price, imageURl, description) VALUES (?,?,?,?)', 
-        [this.title, this.price, this.imageUrl, this.description]);
-    }
-
-    static deleteById(id) {
-
-    }
-    
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
-    }
-
-    static fetchAll(){
-        return db.execute('SELECT * FROM products ');
-    }
-}
+const Product = sequelize.define();
