098f5adccd2f7a657f959e5cdd7144af5a9f735a
Joaquin Oldan <joldan@gmail.com>
2020-09-11 08:09:23 -0300

    added user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+// Import library
+const Sequelize = require('sequelize');
+
+//import sequelize instance for this program
+const sequelize = require('../util/database')
+
+const User = sequelize.define('user',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
--- util/database.js
+++ util/database.js
@@ -1,5 +1,6 @@
 const {Sequelize} = require('sequelize');
 
+//Create sequelize instance, connect it to my DB.
 const sequelize = new Sequelize('node-complete', 'root', 'Adamantio15', {
     dialect: 'mysql',
     host: 'localhost'
