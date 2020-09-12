573deeaec1139613b5df25231bd6e88db64424d8
Joaquin Oldan <joldan@gmail.com>
2020-09-12 17:53:40 -0300

    added Cart and Cart-Item model with sequelize

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const errorController = require('./controllers/errors');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 
 //App creation
@@ -46,8 +49,18 @@ app.use(shopRoutes);
 
 app.use(errorController.controller404);
 
+
+//Define Model Asociation
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+
 
 sequelize
     //.sync( {force: true} )
