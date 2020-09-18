6042d25235f05c25981248ec4a3a4a82e158c34b
Joaquin Oldan <joldan@gmail.com>
2020-09-18 09:38:50 -0300

    added order model

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
-
-
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 //App creation
 const app = express();
@@ -56,10 +56,12 @@ User.hasMany(Product);
 
 User.hasOne(Cart);
 Cart.belongsTo(User);
-
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
 
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 
 sequelize
