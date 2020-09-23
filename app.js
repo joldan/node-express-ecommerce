b19dc8bc92c2ef5873f265ff753b2829c2026c4c
Joaquin Oldan <joldan@gmail.com>
2020-09-23 08:15:56 -0300

    connected to DB

--- app.js
+++ app.js
@@ -8,14 +8,7 @@ const rootDir = require('./util/path');
 
 //Import Controllers
 const errorController = require('./controllers/errors');
-//import DB manager and models
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+
 
 //App creation
 const app = express();
@@ -35,12 +28,12 @@ app.use(express.static(path.join(rootDir, 'public')));
 
 //this midlware adds user 1 to any request
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then( user => {
-            req.user = user;
-            next();
-        })
-        .catch( err => console.log(err));
+    // User.findByPk(1)
+    //     .then( user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch( err => console.log(err));
 })
 
 //Configuring routes
@@ -51,38 +44,3 @@ app.use(errorController.controller404);
 
 
 //Define Model Asociation
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-
-sequelize
-    //.sync( {force: true} )
-    .sync()
-    .then(result => {
-        return User.findByPk(1)
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Joaquin', email: 'something@omdharana.com' })
-        }
-        //values returned inside then blocks are automatically wraped into a promise
-        return user;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => console.log(err));
-
-
--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
-const {Sequelize} = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-//Create sequelize instance, connect it to my DB.
-const sequelize = new Sequelize('node-complete', 'root', 'MySqlFun2020', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
+MongoClient.connect('mongodb+srv://ecomApp:4Bmk82KRrN4rkzyj@node-ecom.zfwd4.mongodb.net/node-ecom?retryWrites=true&w=majority')
+.then(result => {
+    console.log('connected');
+})
+.catch(err => {
+    console.log(err);
+})
