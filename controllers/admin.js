e37fe5a4a4ff90fecf295b8ceb348b269d567021
Joaquin Oldan <joldan@gmail.com>
2020-09-24 08:23:27 -0300

    added edit and delete functions

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
+const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -25,72 +26,62 @@ exports.postAddProduct = (req, res, next) => {
         .catch(err => console.log(err));
 }
 
-// exports.getProducts = (req, res, next) => {
-//     req.user
-//     .getProducts()
-//         .then(products => {
-//             res.render('admin/products', {
-//                 pageTitle: "Admin Products",
-//                 prods: products,
-//                 path: '/admin/products'
-//             });
-//         })
-//         .catch(err => console.log(err))
-// }
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+        .then(products => {
+            res.render('admin/products', {
+                pageTitle: "Admin Products",
+                prods: products,
+                path: '/admin/products'
+            });
+        })
+        .catch(err => console.log(err))
+}
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = req.query.edit;
-//     if (!editMode) {
-//         return res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     req.user
-//         .getProducts({ where: { id: prodId } }) //WHERE id EQUALS prodId
-//         .then(products => {
-//             const product = products[0];
-//             if (!product) {
-//                 return res.redirect('/');
-//             }
-//             res.render('admin/edit-product', {
-//                 pageTitle: 'Edit Product',
-//                 product: product,
-//                 path: '/admin/edit-product',
-//                 editing: editMode
-//             });
-//         })
-//         .catch(err => console.log(err));
-// };
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                product: product,
+                path: '/admin/edit-product',
+                editing: editMode
+            });
+        })
+        .catch(err => console.log(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//     const id = req.body.productId;
-//     const updatedTitle = req.body.title;
-//     const updatedImageUrl = req.body.imageUrl;
-//     const updatedDescription = req.body.description;
-//     const updatedPrice = req.body.price;
-//     Product.findByPk(id)
-//         .then(product => {
-//             product.title = updatedTitle;
-//             product.imageUrl = updatedImageUrl;
-//             product.description = updatedDescription;
-//             product.price = updatedPrice;
-//             return product.save();
-//         })
-//         .then(result => {
-//             console.log('Updated Product!!!')
-//             res.redirect('/admin/products')
-//         })
-//         .catch()
-// }
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedPrice = req.body.price;
 
-// exports.postDeleteProduct = (req, res, next) => {
-//     const id = req.body.productId;
-//     Product.findByPk(id)
-//         .then(product => {
-//             return product.destroy();
-//         })
-//         .then(result => {
-//             console.log("Product Deleted");
-//             res.redirect('/admin/products');
-//         })
-//         .catch(err => { console.log(err) });
-// }
+    const updatedProduct = new Product(updatedTitle, updatedImageUrl, updatedPrice, updatedDescription, prodId);
+    updatedProduct.save()
+        .then(result => {
+            console.log('Updated Product!!!')
+            res.redirect('/admin/products')
+        })
+        .catch()
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    console.log('#############################')
+    const prodId = req.body.productId;
+    console.log(prodId)
+    Product.deleteById(prodId)
+        .then( () => {
+            res.redirect('/admin/products');
+        })
+        .catch(err => { console.log(err) });
+}
--- models/product.js
+++ models/product.js
@@ -1,17 +1,34 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
+
+
 class Product {
-    constructor(title, imageUrl, price, description) {
+    constructor(title, imageUrl, price, description, id) {
         this.title = title;
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
+        this._id = id ? mongodb.ObjectId(id) : null;
     }
 
     save() {
         const db = getDb();
-        return db.collection('products')
-            .insertOne(this)
+        let dbOp;
+        //check previous existence or not
+        if (this._id) {
+            //Update product
+            dbOp = db
+                .collection('products')
+                .updateOne({ _id: this._id }, { $set: this })
+        } else {
+            //Add product
+            dbOp = db
+                .collection('products')
+                .insertOne(this)
+        }
+
+        return dbOp
             .then(result => {
                 console.log(result);
             })
@@ -20,18 +37,38 @@ class Product {
             })
     };
 
-    static fetchAll(){
+    static fetchAll() {
         const db = getDb();
         return db
-        .collection('products')
-        .find()
-        .toArray()
-        .then( products => {
-            return products;
-        })
-        .catch( err => {
-            console.log(err);
-        });
+            .collection('products')
+            .find()
+            .toArray()
+            .then(products => {
+                return products;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(prodId) {
+        const db = getDb();
+        return db.collection('products')
+            .find({ _id: mongodb.ObjectId(prodId) })
+            .next()
+            .then(product => {
+                return product;
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    static deleteById(prodId) {
+        const db = getDb();
+        return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+            .then(result => console.log('Deleted'))
+            .catch(err => console.log(err));
     }
 }
 
